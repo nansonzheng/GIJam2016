59350fc9d98fda74f3db73a762f2f38ad0ccee16
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2016-10-29 16:29:40 -0400

    The moment when you write all the code to do a thing but forget to write the code that does the thing

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -102,7 +102,7 @@ public class Controls : MonoBehaviour {
         }
 
         // Uncomment when ready
-        //setAnimVars();
+        setAnimVars();
     }
 
     // TODO: LayerMask for different objects
@@ -176,6 +176,6 @@ public class Controls : MonoBehaviour {
     void setAnimVars()
     {
         ani.SetFloat("movSpeed", rb.velocity.magnitude);
-        
+        ani.SetFloat("direction", directionF);
     }
 }
