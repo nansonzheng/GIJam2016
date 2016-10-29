30f4683ddcfa01e533ff4b6452c1106bad55b263
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2016-10-28 20:12:32 -0400

    Rudimentary Controls for 2players

---
+++ Assets/Controls.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+    public enum Player {
+        Player1,
+        Player2
+    }
+
+    public Player playerNum;
+    public float speed;
+    public float pushingMultiplier;
+    public bool isPushing;
+    Rigidbody2D rb;
+
+    Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    switch (playerNum)
+        {
+            case Player.Player1:
+                direction.x = Input.GetAxis("Horizontal");
+                direction.y = Input.GetAxis("Vertical");
+                break;
+            case Player.Player2:
+                direction.x = Input.GetAxis("Horizontal2");
+                direction.y = Input.GetAxis("Vertical2");
+                break;
+        }
+
+        rb.velocity = direction * speed;
+	}
+}
