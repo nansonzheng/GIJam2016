ec1fdf7acd2baf82cf92b166bd22a5fa5355b765
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2016-10-28 22:06:30 -0400

    Control changes, physics material

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -12,7 +12,8 @@ public class Controls : MonoBehaviour {
     public float speed;
     public float pushingMultiplier;
     public bool isPushing;
-    Rigidbody2D rb;
+    Rigidbody2D rb, pushed;
+    Vector2 attachNormal;
 
     Vector2 direction;
 
@@ -34,7 +35,44 @@ public class Controls : MonoBehaviour {
                 direction.y = Input.GetAxis("Vertical2");
                 break;
         }
-
         rb.velocity = direction * speed;
+        /*
+        if (!isPushing)
+        {
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            // Determine if moving away
+            float angleDifference = Mathf.Abs(Vector2.Angle(attachNormal, direction));
+            Debug.Log("AngleDifference = " + angleDifference);
+            if (angleDifference < 90)
+                rb.velocity = direction * speed;
+            else
+            {
+                rb.velocity = pushed.velocity = direction * speed * pushingMultiplier;
+            }
+        }
+        */            
 	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (!isPushing) {
+            // TODO: Check collided object is wall or playblock
+            isPushing = true;
+            pushed = col.rigidbody;
+            attachNormal = col.contacts[0].normal;
+            Debug.Log(attachNormal);
+        }
+        // Else, probably being squished
+    }
+
+    void OnCollisionExit2D() {
+        if (isPushing)
+        {
+            isPushing = false;
+            pushed = null;
+            attachNormal = Vector2.zero;
+        }
+    }
 }
---
+++ Assets/PushedBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushedBehaviour : MonoBehaviour {
+
+    Rigidbody2D rb;
+    GameObject pushedBy;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        pushedBy = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (pushedBy == null)
+        {
+            pushedBy = col.gameObject;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject == pushedBy)
+        {
+            pushedBy = null;
+        }
+    }
+}
