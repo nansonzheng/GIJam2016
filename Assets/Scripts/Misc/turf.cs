80a3f62e4bed641ff2d5859181132a765a0d60c2
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-10-29 19:16:46 -0400

    Made a turf coloring

---
+++ Assets/Scripts/Misc/turf.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		//This is supposed to be the block (but its gonna be anything)
+		Color color = col.gameObject.GetComponent<SpriteRenderer> ().color;
+		this.GetComponent<SpriteRenderer> ().color = color;
+	}
+}
