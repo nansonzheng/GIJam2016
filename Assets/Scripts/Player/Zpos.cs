23137f9be1f3b8e4f09432dea3dbce71f82dc4cf
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2016-10-29 18:07:17 -0400

    Z-position modifier script, adjusted player colours for prefab

---
+++ Assets/Scripts/Player/Zpos.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zpos : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+    }
+}
