3392a0101c76b13cd336d931342efd810bedda04
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-10-30 05:00:24 -0400

    Adding new script for turf counting

---
+++ Assets/Scripts/Misc/turfMaster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class turfMaster : MonoBehaviour {
+
+	private IList<GameObject> turf = new List<GameObject>(); 
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			turf.Add (child.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	int countColor(Color color){
+		int counter = 0;
+		foreach (GameObject g in turf) {
+			if (g.GetComponent<SpriteRenderer> ().color == color)
+				counter++;
+		}
+
+		return counter;
+	}
+}
