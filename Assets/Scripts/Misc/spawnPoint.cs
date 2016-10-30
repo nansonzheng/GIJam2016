4696c878b89cec6057066f4d1a843ec074b12dce
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-10-30 13:20:57 -0400

    Added spawn points

--- Assets/Scripts/Misc/spawnPoint.cs
+++ Assets/Scripts/Misc/spawnPoint.cs
@@ -4,15 +4,35 @@ using System.Collections;
 public class spawnPoint : MonoBehaviour {
 
 	public Controls.Player playerNum;
+	public float totalDeathTimer;
+	public float curDeathTimer;
+
 	private GameObject refToPlayer;
 
 	// Use this for initialization
 	void Start () {
-		//spawnPlayer ();
+		spawnIfDeadOrDeathTimer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		refToPlayer = GameObject.Find ("Player"+playerNum);
+		if(refToPlayer == null)
+			spawnIfDeadOrDeathTimer();
+	}
+
+
+	void spawnIfDeadOrDeathTimer(){
+		if (curDeathTimer <= 0) {	//spawn player because penalty is over
+			curDeathTimer = totalDeathTimer;
+			refToPlayer = Instantiate (Resources.Load ("Prefabs/Player/player")) as GameObject;
+
+			//Set some stuff
+			refToPlayer.transform.position = transform.position;
+			refToPlayer.gameObject.GetComponent<Controls> ().playerNum = playerNum;
+			refToPlayer.gameObject.GetComponent<offscreenIndicator> ().playerNum = (int)playerNum;
+		} 
+		else {
+			curDeathTimer -= Time.deltaTime;	//Reduce penalty
+		}
 	}
 }
