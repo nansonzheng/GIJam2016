d4f0ee495f497a156cac7868a51a8e28bfdcbd49
RhainyLeo <liaohenry2310@gmail.com>
2016-10-30 14:38:29 -0400

    added blocksounds script

--- Assets/Scripts/Misc/blockSounds.cs
+++ Assets/Scripts/Misc/blockSounds.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class blockSounds : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject mainCamera;
+    public AudioSource source;
+    public AudioClip bumpSound;
+
+    void OnCollisionEnter2D(Collision col)
+    {
+        if (col.gameObject.CompareTag("Wall") || col.gameObject.CompareTag("Block"))
+        {
+            source.PlayOneShot(bumpSound, 1f);
+            mainCamera.GetComponent<Camera_Shake>().shakeCamera(0.3f, 0.3f);
+        }
+    }
 }
